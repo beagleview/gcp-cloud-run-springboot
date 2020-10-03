9d495da85236fff751fe8f28c20fc94b50db178a
Taechit Chatthamporn <taechit.c@gmail.com>
2020-10-03 08:21:24 +0000

    add find by first name

--- src/main/kotlin/com/bgv/gcp/cloudrun/repository/StudentReposutory.kt
+++ src/main/kotlin/com/bgv/gcp/cloudrun/repository/StudentReposutory.kt
@@ -2,4 +2,6 @@ package com.bgv.gcp.cloudrun.repository
 
 import org.springframework.data.repository.reactive.ReactiveCrudRepository
 
-interface EventRepository : ReactiveMongoRepository
+interface EventReposidtory : ReactiveMongoRepository<Student:String>{
+    fun findByFirstName(firstName: String): Mono(Student)
+}
