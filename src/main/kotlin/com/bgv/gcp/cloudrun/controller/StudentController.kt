a38308d48fb479b0f4c247baf797070243525902
Taechit Chatthamporn <taechit.c@gmail.com>
2020-10-04 16:31:13 +0700

    [UPDATE] controller add get/post method

--- src/main/kotlin/com/bgv/gcp/cloudrun/controller/StudentController.kt
+++ src/main/kotlin/com/bgv/gcp/cloudrun/controller/StudentController.kt
@@ -1,8 +1,36 @@
 package com.bgv.gcp.cloudrun.controller
 
-import org.springframework.web.bind.annotation.RestController
+import com.bgv.gcp.cloudrun.service.StudentService
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.*
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+import com.bgv.gcp.cloudrun.model.Student
 
 @RestController
+@RequestMapping("/api/student")
 class StudentController{
- 
+    @Autowired
+    lateinit var studentService: StudentService
+
+    val logger = LoggerFactory.getLogger(this.javaClass)
+
+    @PostMapping("/")
+    fun create(@RequestBody student: Student): Mono<Student> {
+        logger.info("[save student information] request parameter: {}, student")
+                return studentService.create(student)
+    }
+
+    @GetMapping("/list")
+    fun listStudent(): Flux<Student> {
+        return studentService.list()
+    }
+
+    @GetMapping("/id")
+    fun student(@RequestParam id : String): Mono<Student> {
+        logger.info("Query Student Number: {}, id")
+                return studentService.find(id)
+    }
 }
